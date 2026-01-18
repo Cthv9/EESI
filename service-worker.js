7d1e3df5264a27b7ca31cf53aeeafd64331eb2bd
Davide <service.davidesaim@gmail.com>
2026-01-18 17:53:52 +0100

    Initial

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'isee-2026-cache-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/isee_icon.ico',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve()))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  // Cache-first for same-origin + CDN resources
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const copy = res.clone();
+        // Only cache successful responses
+        if (res.ok) {
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(()=>{});
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
